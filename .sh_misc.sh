bf90e8cae91da4562e2eba27107760009ad443c4
olsonpm <philip.olson@protonmail.ch>
2016-01-10 14:31:17 -0600

    Add more miscellaneous shell settings as well as a couple variables

--- .sh_misc.sh
+++ .sh_misc.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env sh
 
 export GIT_PAGER='';
+
+setopt extendedglob
+
+xrdb -merge $HOME/.Xresources
+
+bindkey "\033[;55" backward-kill-word
+bindkey "\033[53" kill-word
+bindkey "\C-k" kill-whole-line
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\e[7~" beginning-of-line
+bindkey "\e[8~" end-of-line
+bindkey "\eOH" beginning-of-line
+bindkey "\eOF" end-of-line
+bindkey "\e[H" beginning-of-line
+bindkey "\e[F" end-of-line
