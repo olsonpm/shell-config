71ba4d1f08070527e7e6fd136e2c1864434111c6
philip olson <polson@amfam.com>
2016-01-22 11:20:03 -0600

    Restructure files so that we have file extensions

---
+++ lib/suspend.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env sh
+
+dbus-send --print-reply\
+    --system\
+    --dest=org.freedesktop.login1\
+    /org/freedesktop/login1\
+    org.freedesktop.login1.Manager.Suspend\
+    boolean:true
