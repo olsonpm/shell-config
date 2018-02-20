a3e6235ad9ad8c75a3d0d14b87ce6bd6f5cf6837
Philip Olson <philip.olson@protonmail.ch>
2018-02-19 23:28:24 -0600

    misc update

--- .sh_misc.sh
+++ .sh_misc.sh
@@ -6,7 +6,7 @@ setopt extendedglob
 
 alias git-list-untracked='git ls-files --others --exclude-standard'
 
-bindkey "\x08" backward-kill-word
+bindkey '^H' backward-kill-word
 bindkey '^[[3;5~' kill-word
 bindkey '^k' kill-whole-line
 bindkey '^u' backward-kill-line
@@ -14,26 +14,8 @@ bindkey '^[OH' beginning-of-line
 bindkey '^[OF' end-of-line
 bindkey '^I' expand-or-complete
 
-# I+Ctrl should be mapped to '\x02' (control + b).  This is to get around tab's
-#   escape sequence being '^I'.
-bindkey "\x02" kill-line
-
-#autoload -U add-zsh-hook
-load_nvmrc() {
-  if [ -f .nvmrc ] && [ -r .nvmrc ]; then
-    nvm use
-  fi
-}
-#add-zsh-hook chpwd load_nvmrc
-
-load_jenv() {
-  if [ -s "/home/phil/.jenv/bin/jenv-init.sh" ]; then
-    . "/home/phil/.jenv/bin/jenv-init.sh"
-    . "/home/phil/.jenv/commands/completion.sh"
-  fi
-}
-
 alias jqd='jq .dependencies package.json'
+alias jqs='jq .scripts package.json'
 
 # NSS related env vars
 export NSS_DEFAULT_DB_TYPE='sql:'
