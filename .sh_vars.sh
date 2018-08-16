6b700697c04a9881e243a6acdf85ae5a6b629c49
Philip Olson <philip.olson@pm.me>
2018-08-16 10:20:44 -0500

    remove docean and add pr

--- .sh_vars.sh
+++ .sh_vars.sh
@@ -4,5 +4,5 @@ export personal="${HOME}/git-repos/personal"
 export tmp="${HOME}/tmp/npm-tmp"
 export forked="${HOME}/git-repos/forked"
 export cloned="${HOME}/git-repos/cloned"
-export docean="104.236.42.94"
+export pr="${HOME}/git-repos/pr"
 export PATH=$HOME/bin:$PATH
