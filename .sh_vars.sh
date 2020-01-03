37ec5a6ce3823c7a642c9ffe79cb0eb00e793065
Philip Olson <phil.olson@swarmsolutions.com>
2020-01-02 19:57:12 -0600

    add work git-repo

--- .sh_vars.sh
+++ .sh_vars.sh
@@ -4,5 +4,6 @@ export personal="${HOME}/git-repos/personal"
 export tmp="${HOME}/tmp"
 export forked="${HOME}/git-repos/forked"
 export cloned="${HOME}/git-repos/cloned"
+export work="${HOME}/git-repos/work"
 export pr="${HOME}/git-repos/pr"
 export PATH=$HOME/bin:$PATH
